e5077d47840992e1494b7b510e4ad4ec0cebde33
myphoenix2022 <pkumarphilly@gmail.com>
2022-02-18 16:48:05 -0500

    Fixing More issues

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - this is Sixth testing comment
+          Learn React - this is Seventh testing comment
         </a>
       </header>
     </div>
